5a12ee6942621bf0ad540b16bddb81d37a9e9c91
You-Ri Su <suyourice@gmail.com>
2022-05-28 22:25:35 +0900

    Experiment and results

--- DBE/EncodingPower/CountingAndPower.py
+++ DBE/EncodingPower/CountingAndPower.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.stdout = open('/home/youri/project/gem5/DBE/ExperimentResults.txt', 'w')
+
 def inversion(_data):
     for d in range(len(_data)):
 
@@ -16,7 +20,7 @@ def calRatio(_after, _before):
 def calCharging(_after, _before):
     charging = 0
     diff = _after - _before
-    print('diff = ', diff)
+    # print('diff = ', diff)
     if diff == 1:
         charging = 1/6
     elif diff == 2:
@@ -28,13 +32,13 @@ def calCharging(_after, _before):
 
 def sortCompare(_origin, _sorted):
     _cList = [] # to store the index if it is changed : [index, diff]
-    print('origin = ', _origin)
-    print('sorted = ', _sorted)
+    # print('origin = ', _origin)
+    # print('sorted = ', _sorted)
     for _index in range(4):
         if _origin[_index] < _sorted[_index]:
             _diff = _sorted[_index] - _origin[_index]
             _cList.append([_index, _diff])
-    print('cList = ', _cList)
+    # print('cList = ', _cList)
 
     return _cList
 
@@ -42,7 +46,7 @@ def copyInit(_originFlag):
     iniFlags = []
     for i in _originFlag:
         iniFlags.append(i)
-    print('iniFlags = ', iniFlags)
+    # print('iniFlags = ', iniFlags)
 
     return iniFlags
 
@@ -90,21 +94,21 @@ class Flag:
 
 
 # read files
-# data_1_bin = open("/home/youri/project/gem5/DBE/DATA_input/1_bin.txt","r")
-# data_2_bin = open("/home/youri/project/gem5/DBE/DATA_input/2_bin.txt","r")
-# data_3_bin = open("/home/youri/project/gem5/DBE/DATA_input/3_bin.txt","r")
-# data_4_bin = open("/home/youri/project/gem5/DBE/DATA_input/4_bin.txt","r")
-# data_5_bin = open("/home/youri/project/gem5/DBE/DATA_input/5_bin.txt","r")
-# data_6_bin = open("/home/youri/project/gem5/DBE/DATA_input/6_bin.txt","r")
-# data_7_bin = open("/home/youri/project/gem5/DBE/DATA_input/7_bin.txt","r")
-# data_8_bin = open("/home/youri/project/gem5/DBE/DATA_input/8_bin.txt","r")
-# data_9_bin = open("/home/youri/project/gem5/DBE/DATA_input/9_bin.txt","r")
-# data_10_bin = open("/home/youri/project/gem5/DBE/DATA_input/10_bin.txt","r")
-# data_11_bin = open("/home/youri/project/gem5/DBE/DATA_input/11_bin.txt","r")
-# data_12_bin = open("/home/youri/project/gem5/DBE/DATA_input/12_bin.txt","r")
-# data_13_bin = open("/home/youri/project/gem5/DBE/DATA_input/13_bin.txt","r")
+data_1_bin = open("/home/youri/project/gem5/DBE/DATA_input/1_bin.txt","r")
+data_2_bin = open("/home/youri/project/gem5/DBE/DATA_input/2_bin.txt","r")
+data_3_bin = open("/home/youri/project/gem5/DBE/DATA_input/3_bin.txt","r")
+data_4_bin = open("/home/youri/project/gem5/DBE/DATA_input/4_bin.txt","r")
+data_5_bin = open("/home/youri/project/gem5/DBE/DATA_input/5_bin.txt","r")
+data_6_bin = open("/home/youri/project/gem5/DBE/DATA_input/6_bin.txt","r")
+data_7_bin = open("/home/youri/project/gem5/DBE/DATA_input/7_bin.txt","r")
+data_8_bin = open("/home/youri/project/gem5/DBE/DATA_input/8_bin.txt","r")
+data_9_bin = open("/home/youri/project/gem5/DBE/DATA_input/9_bin.txt","r")
+data_10_bin = open("/home/youri/project/gem5/DBE/DATA_input/10_bin.txt","r")
+data_11_bin = open("/home/youri/project/gem5/DBE/DATA_input/11_bin.txt","r")
+data_12_bin = open("/home/youri/project/gem5/DBE/DATA_input/12_bin.txt","r")
+data_13_bin = open("/home/youri/project/gem5/DBE/DATA_input/13_bin.txt","r")
 data_14_bin = open("/home/youri/project/gem5/DBE/DATA_input/14_bin.txt","r")
-# data_random = open("/home/youri/project/gem5/DBE/DATA_input/random_data_10000.txt","r")
+data_random = open("/home/youri/project/gem5/DBE/DATA_input/random_data_10000.txt","r")
 
 def doExperiment(_file,_num):
     print('********************* start experiment ', _num, ' *********************')
@@ -175,33 +179,33 @@ def doExperiment(_file,_num):
 
         # encoded
         DBI_Flag_Origin = copyInit(DBI_Flag.Flags) # to store the initial state
-        print('DBI_Flag_Origin = ', DBI_Flag_Origin)
+        # print('DBI_Flag_Origin = ', DBI_Flag_Origin)
 
         ### 2
         if 3 * DBI_Flag.Flags[0] + DBI_Flag.Flags[1] - DBI_Flag.Flags[2] - 3 * DBI_Flag.Flags[3] > 0:   # before power > after power => inversion
             total_powerdc_2 = total_powerdc_2 + DBI_Flag.calPower(3,2,1,0)
             charging_2 = charging_2 + calCharging(3,0) * DBI_Flag.Flags[0] + calCharging(2,1) * DBI_Flag.Flags[1] ## 00 -> 11 and 01 -> 10
-            print('charging_2 = ', charging_2)
-            print('DBI_Flag.Flags[0] = ',DBI_Flag.Flags[0])
-            print('calCharging(3,0) = ',calCharging(3,0))
+            # print('charging_2 = ', charging_2)
+            # print('DBI_Flag.Flags[0] = ',DBI_Flag.Flags[0])
+            # print('calCharging(3,0) = ',calCharging(3,0))
         else:
             total_powerdc_2 = total_powerdc_2 + DBI_Flag.calPower(0,1,2,3)
-        print('after 2 = ', DBI_Flag.Flags)
+        # print('after 2 = ', DBI_Flag.Flags)
         ### 3
         changed_DBI_Flag, maxIndex = DBI_Flag.change_11()
-        print('change_11 =', DBI_Flag.Flags)
+        # print('change_11 =', DBI_Flag.Flags)
         total_powerdc_3 = total_powerdc_3 + changed_DBI_Flag.calPower(0,1,2,3)
         charging_3 = charging_3 + calCharging(3,maxIndex) * DBI_Flag.Flags[maxIndex]
         ### 4
         sorted_DBI_Flag = DBI_Flag.sortFlags()
-        print('sorted4 = ',DBI_Flag.Flags)
+        # print('sorted4 = ',DBI_Flag.Flags)
         total_powerdc_4 = total_powerdc_4 + sorted_DBI_Flag.calPower(0,1,2,3)
-        print('DBI_Flag_Origin', DBI_Flag_Origin)
+        # print('DBI_Flag_Origin', DBI_Flag_Origin)
         cList = sortCompare(DBI_Flag_Origin, sorted_DBI_Flag.Flags)
         for i in range(len(cList)):
             charging_4 = charging_4 + calCharging(cList[i][1], 0) * DBI_Flag.Flags[cList[i][0]]
         _count = 1
-        print('\n\n\n\n\n')
+        # print('\n\n\n\n\n')
 
     # Ratio
     print('######################### Result ', _num, '#########################') 
@@ -224,46 +228,48 @@ def doExperiment(_file,_num):
     print("total number [00]: ", num_flag[0] / total_flag * 100, "%  /  [01]: ",  num_flag[1] / total_flag * 100, "%  /  [10]: ",  num_flag[2] / total_flag * 100, "%  /  [11]: ",  num_flag[3] / total_flag * 100, "%\n\n\n")
     
     # Power
-    print("\n\n\n******************  SWITCHING POWER  ******************\n\n\n")
+    print("******************  SWITCHING POWER  ******************\n\n\n")
     print("NRZ-DBI Power = ", charging_1 * const, "W")
     print("PAM4-DBI Power = ", charging_2 * const, "W")
     print("PAM4-MF Power = ", charging_3 * const, "W")
-    print("PAM4-Sort Power = ", charging_4 * const, "W")
+    print("PAM4-Sort Power = ", charging_4 * const, "W\n\n\n\n\n")
 
 
-# doExperiment(data_1_bin,1)
-# doExperiment(data_2_bin,2)
-# doExperiment(data_3_bin,3)
-# doExperiment(data_4_bin,4)
-# doExperiment(data_5_bin,5)
-# doExperiment(data_6_bin,6)
-# doExperiment(data_7_bin,7)
-# doExperiment(data_8_bin,8)
-# doExperiment(data_9_bin,9)
-# doExperiment(data_10_bin,10)
-# doExperiment(data_11_bin,11)
-# doExperiment(data_12_bin,12)
-# doExperiment(data_13_bin,13)
+doExperiment(data_1_bin,1)
+doExperiment(data_2_bin,2)
+doExperiment(data_3_bin,3)
+doExperiment(data_4_bin,4)
+doExperiment(data_5_bin,5)
+doExperiment(data_6_bin,6)
+doExperiment(data_7_bin,7)
+doExperiment(data_8_bin,8)
+doExperiment(data_9_bin,9)
+doExperiment(data_10_bin,10)
+doExperiment(data_11_bin,11)
+doExperiment(data_12_bin,12)
+doExperiment(data_13_bin,13)
 doExperiment(data_14_bin,14)
-# doExperiment(data_random,15)
+doExperiment(data_random,15)
 
 
 
 
-# data_1_bin.close()
-# data_2_bin.close()
-# data_3_bin.close()
-# data_4_bin.close()
-# data_5_bin.close()
-# data_6_bin.close()
-# data_7_bin.close()
-# data_8_bin.close()
-# data_9_bin.close()
-# data_10_bin.close()
-# data_11_bin.close()
-# data_12_bin.close()
-# data_13_bin.close()
+data_1_bin.close()
+data_2_bin.close()
+data_3_bin.close()
+data_4_bin.close()
+data_5_bin.close()
+data_6_bin.close()
+data_7_bin.close()
+data_8_bin.close()
+data_9_bin.close()
+data_10_bin.close()
+data_11_bin.close()
+data_12_bin.close()
+data_13_bin.close()
 data_14_bin.close()
-# data_random.close()
+data_random.close()
 
 print('clear')
+
+sys.stdout.close()
