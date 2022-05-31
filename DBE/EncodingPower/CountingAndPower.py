3dfaf0b1016c2b728fb5fa3be84b4f9601ed9e5d
You-Ri Su <suyourice@gmail.com>
2022-05-31 13:20:56 +0900

    total # of cycle added

--- DBE/EncodingPower/CountingAndPower.py
+++ DBE/EncodingPower/CountingAndPower.py
@@ -148,7 +148,7 @@ def doExperiment(_file,_num):
         # encoded
         ### 1
         if sum(data_NRZ) <= 4:
-            charging_1 = charging_1 + (8 - sum(data_NRZ))
+            charging_1 = charging_1 + (8 - sum(data_NRZ)) / 8
             inversion(data_NRZ)
         current_powerdc_1 = 1/100 * (8-sum(data_NRZ))
         total_powerdc_1 = total_powerdc_1 + current_powerdc_1
@@ -185,7 +185,7 @@ def doExperiment(_file,_num):
         ### 2
         if 3 * DBI_Flag.Flags[0] + DBI_Flag.Flags[1] - DBI_Flag.Flags[2] - 3 * DBI_Flag.Flags[3] > 0:   # before power > after power => inversion
             total_powerdc_2 = total_powerdc_2 + DBI_Flag.calPower(3,2,1,0)
-            charging_2 = charging_2 + calCharging(3,0) * DBI_Flag.Flags[0] + calCharging(2,1) * DBI_Flag.Flags[1] ## 00 -> 11 and 01 -> 10
+            charging_2 = charging_2 + (calCharging(3,0) * DBI_Flag.Flags[0] + calCharging(2,1) * DBI_Flag.Flags[1]) / 8 ## 00 -> 11 and 01 -> 10
             # print('charging_2 = ', charging_2)
             # print('DBI_Flag.Flags[0] = ',DBI_Flag.Flags[0])
             # print('calCharging(3,0) = ',calCharging(3,0))
@@ -197,7 +197,7 @@ def doExperiment(_file,_num):
         # print('change_11 =', DBI_Flag.Flags)
         total_powerdc_3 = total_powerdc_3 + changed_DBI_Flag.calPower(0,1,2,3)
         # print(DBI_Flag.Flags)
-        charging_3 = charging_3 + calCharging(3,maxIndex) * DBI_Flag.Flags[3]
+        charging_3 = charging_3 + calCharging(3,maxIndex) * DBI_Flag.Flags[3] / 8
         # print(calCharging(3,maxIndex))
         ### 4
         sorted_DBI_Flag = DBI_Flag.sortFlags()
@@ -206,7 +206,7 @@ def doExperiment(_file,_num):
         # print('DBI_Flag_Origin', DBI_Flag_Origin)
         cList = sortCompare(DBI_Flag_Origin, sorted_DBI_Flag.Flags)
         for i in range(len(cList)):
-            charging_4 = charging_4 + calCharging(cList[i][1], 0) * DBI_Flag.Flags[cList[i][0]]
+            charging_4 = charging_4 + calCharging(cList[i][1], 0) * DBI_Flag.Flags[cList[i][0]] / 8
         _count = 1
         # print('\n\n\n\n\n')
 
