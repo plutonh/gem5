e460879bc18a002158164350fcf96823317d272c
You-Ri Su <suyourice@gmail.com>
2022-06-01 16:47:20 +0900

    more information

--- DBE/EncodingPower/CountingAndPower.py
+++ DBE/EncodingPower/CountingAndPower.py
@@ -133,9 +133,17 @@ def doExperiment(_file,_num):
     lines = _file.readlines()
 
     const = 15 * 3.3 * 10e-3 / (len(lines) * 8) # 15pF * 3.3GHz * (1V)^2
+    const_NRZ = 15 * 3.3 * 10e-3 / 8
+    const_PAM4 = 15 * 3.3 * 10e-3 / 16
+    print('experiment ', _num, 'has ', len(lines), 'NRZ words and ', len(lines)/2, 'PAM4 words')
 
     example_1 = 0
     example_2 = 0
+
+    eCount_1 = 0
+    eCount_2 = 0
+    eCount_3 = 0
+    eCount_4 = 0
 
     # p = 0
 
@@ -154,10 +162,11 @@ def doExperiment(_file,_num):
             chargeCount = (8 - sum(data_NRZ))
             charging_1 = charging_1 + chargeCount
             inversion(data_NRZ)
+            eCount_1 = eCount_1 + 1
             if example_1 < 3:
                 print('#1 NRZ-DBI')
                 print(lines[i], ' encoded to ', data_NRZ)
-                print('switching power: ', chargeCount, ' * ', const, ' = ', chargeCount * const)
+                print('switching power: ', chargeCount, ' * ', const_NRZ, ' = ', chargeCount * const_NRZ)
         elif example_1 < 3:
             print('#1 NRZ-DBI')
             print(lines[i], " didn't encoded")
@@ -195,17 +204,18 @@ def doExperiment(_file,_num):
         DBI_Flag_Origin = copyInit(DBI_Flag.Flags) # to store the initial state
         if example_2 < 3:
             print('PAM4 data =')
-            print(lines[i])
-            print(lines[i+1])
+            print(data_PAM4[0])
+            print(data_PAM4[1])
             print('Origin Flags: ', DBI_Flag_Origin)
 
         ### 2
         if 3 * DBI_Flag.Flags[0] + DBI_Flag.Flags[1] - DBI_Flag.Flags[2] - 3 * DBI_Flag.Flags[3] > 0:   # before power > after power => inversion
             total_powerdc_2 = total_powerdc_2 + DBI_Flag.calPower(3,2,1,0)
             charging_2 = charging_2 + (calCharging(3,0) * DBI_Flag.Flags[0] + calCharging(2,1) * DBI_Flag.Flags[1]) ## 00 -> 11 and 01 -> 10
+            eCount_2 = eCount_2 + 1
             if example_2 < 3:
                 print('#2 PAM4-DBI')
-                print('charging 2: ', '(', calCharging(3,0), ' * ', DBI_Flag.Flags[0], ' + ', calCharging(2,1), ' * ', DBI_Flag.Flags[1], ')', ' * ', const, ' = ', (calCharging(3,0) * DBI_Flag.Flags[0] + calCharging(2,1) * DBI_Flag.Flags[1]) * const)
+                print('charging 2: ', '(', calCharging(3,0), ' * ', DBI_Flag.Flags[0], ' + ', calCharging(2,1), ' * ', DBI_Flag.Flags[1], ')', ' * ', const_PAM4, ' = ', (calCharging(3,0) * DBI_Flag.Flags[0] + calCharging(2,1) * DBI_Flag.Flags[1]) * const_PAM4)
             # print('charging_2 = ', charging_2)
             # print('DBI_Flag.Flags[0] = ',DBI_Flag.Flags[0])
             # print('calCharging(3,0) = ',calCharging(3,0))
@@ -220,7 +230,7 @@ def doExperiment(_file,_num):
         if example_2 < 3:
             print('#3 PAM4-MF')
             print('change_11 =', DBI_Flag.Flags)
-            print('charging 3: ', calCharging(3,maxIndex),' * ', DBI_Flag.Flags[3], ' * ', const, ' = ', calCharging(3,maxIndex) * DBI_Flag.Flags[3] * const)
+            print('charging 3: ', calCharging(3,maxIndex),' * ', DBI_Flag.Flags[3], ' * ', const, ' = ', calCharging(3,maxIndex) * DBI_Flag.Flags[3] * const_PAM4)
         total_powerdc_3 = total_powerdc_3 + changed_DBI_Flag.calPower(0,1,2,3)
         # print(DBI_Flag.Flags)
         charging_3 = charging_3 + calCharging(3,maxIndex) * DBI_Flag.Flags[3]
@@ -231,15 +241,17 @@ def doExperiment(_file,_num):
         total_powerdc_4 = total_powerdc_4 + sorted_DBI_Flag.calPower(0,1,2,3)
         # print('DBI_Flag_Origin', DBI_Flag_Origin)
         cList = sortCompare(DBI_Flag_Origin, sorted_DBI_Flag.Flags)
+        if example_2 < 3:
+            print('#4 PAM4-Sort')
         for i in range(len(cList)):
             charging_4 = charging_4 + calCharging(cList[i][1], 0) * DBI_Flag.Flags[cList[i][0]]
             if example_2 < 3:
-                print('#4 PAM4-Sort')
                 print('sorted = ', DBI_Flag.Flags)
-                print('charging #', i, ': ', calCharging(cList[i][1], 0), ' * ', DBI_Flag.Flags[cList[i][0]], ' * ', ' = ', calCharging(cList[i][1], 0) * DBI_Flag.Flags[cList[i][0]] * const, '\n\n\n')
+                print('charging #', i, ': ', calCharging(cList[i][1], 0), ' * ', DBI_Flag.Flags[cList[i][0]], ' * ', const_PAM4, ' = ', calCharging(cList[i][1], 0) * DBI_Flag.Flags[cList[i][0]] * const_PAM4)
         _count = 1
         # print('\n\n\n\n\n')
-        exmaple_2 = example_2 + 1
+        if example_2 < 3:
+            example_2 = example_2 + 1
 
     # Ratio
     print('######################### Result ', _num, '#########################') 
@@ -263,8 +275,8 @@ def doExperiment(_file,_num):
     
     # Power
     print("******************  SWITCHING POWER  ******************\n\n\n")
-    print("NRZ-DBI Power = ", charging_1 * const, "W")
-    print("PAM4-DBI Power = ", charging_2 * const, "W")
+    print("NRZ-DBI Power = ", charging_1 * const, "W     encoded ", eCount_1, ' times')
+    print("PAM4-DBI Power = ", charging_2 * const, "W     encoded ", eCount_2, ' times')
     print("PAM4-MF Power = ", charging_3 * const, "W")
     print("PAM4-Sort Power = ", charging_4 * const, "W\n\n\n\n\n")
 
